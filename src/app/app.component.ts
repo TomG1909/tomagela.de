00dfbee2505f820fccc521e2d7d16e1497d99b63
Toma Gela <toma.gela@gmail.com>
2022-02-01 16:07:06 +0100

    added images and startet to work on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tomagela.de';
+export class AppComponent implements OnInit {
+  images = ['code.jpg', 'notebook.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage() {
+
+    setInterval(() => {
+
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+
+    }, 8000)
+
+
+
+  }
+
+
+
 }
+
