16f8470fe2019ca3314c677e2bb3e0820bfe18de
Toma Gela <toma.gela@gmail.com>
2022-02-02 13:25:30 +0100

    added  menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,36 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['code.jpg', 'notebook.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-
-  updateImage() {
-
-    setInterval(() => {
-
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-
-    }, 8000)
-
-
-
-  }
+export class AppComponent {
 
 
 
